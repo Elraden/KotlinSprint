2fb0561e6582f70bf6d84c755e46011e92b6de56
Elraden <morozovsa94@yandex.ru>
2024-12-17 08:51:29 +0300

    KS-2-4 changed a variable to a constant

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
 
+const val PERCENT_100 = 100f
+
 fun main() {
     val crystalOre = 7
     val ironOre = 10
-
     val buff = 20
-    val crystalOreWithBuff = crystalOre + (crystalOre * (buff / 100f))
-    val ironOreWithBuff = ironOre + (ironOre * (buff / 100f))
+    val crystalOreWithBuff = crystalOre + (crystalOre * (buff / PERCENT_100))
+    val ironOreWithBuff = ironOre + (ironOre * (buff / PERCENT_100))
 
     println("Кристальная руда с бонусом: ${crystalOreWithBuff.toInt()}")
     println("Железная руда с бонусом: ${ironOreWithBuff.toInt()}")
