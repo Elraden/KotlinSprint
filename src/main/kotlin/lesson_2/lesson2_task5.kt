b5a2849a8fc89b3c3c4c691b3050abae21d8b388
Elraden <morozovsa94@yandex.ru>
2024-12-17 09:10:13 +0300

    KS-2-5 changed a variables to a constants

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,16 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val INTEREST_RATE = 16.7
+const val PERCENT_DIVISOR = 100
+const val BASE_RATE = 1.0
+
 fun main() {
     val initialDeposit = 70000
-    val rate = 16.7 / 100
+    val rate = INTEREST_RATE / PERCENT_DIVISOR
     val years = 20
 
-    val finalAmount = initialDeposit * (1 + rate).pow(years)
+    val finalAmount = initialDeposit * (BASE_RATE + rate).pow(years)
 
     val formattedAmount = String.format("%.3f", finalAmount)
 
