acad83bf9f8fc8534edd44876d2e37b06651ee77
Elraden <morozovsa94@yandex.ru>
2024-12-17 08:46:22 +0300

    KS-2-3 changed a variable to a constant

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,16 @@
 package org.example.lesson_2
 
+const val MINUTES_PER_HOUR = 60
+
 fun main() {
-    val departureHour = 9
     val departureMinutes = 39
     val travelTime = 457
+    val departureHour = 9
 
-    val departureTimeMinutes = departureHour * 60 + departureMinutes
+    val departureTimeMinutes = departureHour * MINUTES_PER_HOUR + departureMinutes
     val arrivalFullTime = departureTimeMinutes + travelTime
 
-    val arrivalHour = arrivalFullTime / 60
-    val arrivalMinute = arrivalFullTime % 60
+    val arrivalHour = arrivalFullTime / MINUTES_PER_HOUR
+    val arrivalMinute = arrivalFullTime % MINUTES_PER_HOUR
     println("$arrivalHour:$arrivalMinute")
 }
