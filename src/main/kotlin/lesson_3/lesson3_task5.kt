e5629fe02583f2d3908f1a7f550701ade5b1d275
Elraden <morozovsa94@yandex.ru>
2024-12-18 22:08:56 +0300

    KS-3-5 refactor code, add split method

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,9 +2,12 @@ package org.example.lesson_3
 
 fun main() {
     val moveString = "D2-D4;0"
-    val from = moveString.substring(0,2)
-    val to = moveString.substring(3,5)
-    val moveNumber = moveString.substring(6,7)
+
+    val splitedMoveString = moveString.split("-", ";")
+
+    val from = splitedMoveString[0]
+    val to = splitedMoveString[1]
+    val moveNumber = splitedMoveString[2]
 
     println(from)
     println(to)
