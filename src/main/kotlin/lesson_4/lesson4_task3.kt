e6302509f41f4f2f99d8de6db8c71d5e363b41f6
Elraden <morozovsa94@yandex.ru>
2024-12-19 08:48:30 +0300

    KS-4-3 added criteria in constants, added initialized variables

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,17 +1,21 @@
 package org.example.lesson_4
 
+
+const val REQUIRED_SUNNY_WEATHER = true
+const val REQUIRED_TENT_STATE = true
+const val REQUIRED_HUMIDITY_PERCENT = 20
+const val UNFAVORABLE_SEASON = "зима"
+
 fun main() {
     val isSunnyToday = true
     val isTentOpen = true
     val humidityPercent = 20
     val currentSeason = "зима"
-    val optimalHumidityPercent = 20
-    val unfavorableSeason = "зима"
 
-    val optimalGrowthConditions = isSunnyToday &&
-                                  isTentOpen &&
-                                  (humidityPercent == optimalHumidityPercent) &&
-                                  (currentSeason != unfavorableSeason)
+    val optimalGrowthConditions = (isSunnyToday == REQUIRED_SUNNY_WEATHER) &&
+                                  (isTentOpen == REQUIRED_TENT_STATE) &&
+                                  (humidityPercent == REQUIRED_HUMIDITY_PERCENT) &&
+                                  (currentSeason != UNFAVORABLE_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $optimalGrowthConditions")
 
