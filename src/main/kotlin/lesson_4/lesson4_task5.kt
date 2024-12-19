5c7baefad94e9326031abc76c1e5529319335121
Elraden <morozovsa94@yandex.ru>
2024-12-19 09:05:31 +0300

    KS-4-5 removed unnecessary variables, added full condition

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,27 +1,32 @@
 package org.example.lesson_4
 
 fun main() {
+    println("Не имеется ли повреждений корпуса (true - не имеется / false - имеется)?")
     val isCaseNotDamaged = readln().toBoolean()
+
+    println("Каков текущий состав экипажа?")
     val crewNumber = readln().toInt()
+
+    println("Сколько ящиков с провизией на борту?")
     val boxesQuantity = readln().toInt()
+
+    println("Благоприятная ли погода? (true - благоприятная / false - неблагоприятная)")
     val isWeatherFavourable = readln().toBoolean()
 
     val minCrewNumber = 55
     val maxCrewNumber = 70
-    val crewNumberRange = minCrewNumber..maxCrewNumber
     val minBoxesQuantity = 50
-    val recommendedCrewNumber = crewNumber in crewNumberRange
-    val recommendedBoxesQuantity = boxesQuantity > minBoxesQuantity
 
-    val favourableCondition = recommendedBoxesQuantity &&
-                              recommendedCrewNumber &&
-                              isWeatherFavourable &&
-                              isCaseNotDamaged
+    val canShipSail = (
+            (boxesQuantity > minBoxesQuantity &&
+            crewNumber in minCrewNumber..maxCrewNumber &&
+            isWeatherFavourable &&
+            isCaseNotDamaged) ||
+            (!isCaseNotDamaged &&
+            crewNumber == maxCrewNumber &&
+            isWeatherFavourable &&
+            boxesQuantity >= minBoxesQuantity)
+            )
 
-    val alternateCondition = !isCaseNotDamaged &&
-                             crewNumber == maxCrewNumber &&
-                             isWeatherFavourable &&
-                             boxesQuantity >= minBoxesQuantity
-
-    println("Корабль может отправиться в плавание: ${favourableCondition || alternateCondition} ")
+    println("Корабль может отправиться в плавание: $canShipSail")
 }
