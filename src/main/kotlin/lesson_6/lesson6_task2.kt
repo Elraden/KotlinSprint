6e6b02531399f9480703a7bf4e718021fe657482
Elraden <morozovsa94@yandex.ru>
2024-12-26 19:55:27 +0300

    KS-6-2 fixed mistakes

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,13 +3,8 @@ package org.example.lesson_6
 const val MILLISECONDS_IN_SECOND = 1000
 fun main() {
     println("Введите количество секунд")
-    val seconds = readln().toInt()
-    var counter = 0
-    do {
-        Thread.sleep(MILLISECONDS_IN_SECOND.toLong())
-        counter++
-        if (counter == seconds) {
-            println("Прошло $counter секунд")
-        }
-    } while (counter < seconds)
+    val seconds = readln().toLong()
+    val secondsToMilliseconds = seconds * MILLISECONDS_IN_SECOND
+    Thread.sleep(secondsToMilliseconds)
+    println("Прошло $seconds секунд")
 }
