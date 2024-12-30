47b382cbec4f8787548f1067af527a947741aa22
Elraden <morozovsa94@yandex.ru>
2024-12-30 21:53:16 +0300

    KS-9-4 fix mistakes

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -3,7 +3,6 @@ package org.example.lesson_9
 fun main() {
     println("Введите 5 ингредиентов через запятую с пробелом")
     val userIngredients = readln()
-    val splitIngredients = userIngredients.split(", ")
-    val sortedIngredients = splitIngredients.sorted()
-    println(sortedIngredients)
+    val splitAndSortedIngredients = userIngredients.split(", ").sorted()
+    println(splitAndSortedIngredients)
 }
