b14aba21c1cd59c6747dc431c697db11486db244
Elraden <morozovsa94@yandex.ru>
2024-12-30 21:50:50 +0300

    KS-9-3 fix mistakes

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -4,12 +4,10 @@ fun main() {
     val ingredientsCountList = mutableListOf(2, 50, 15)
     println("Сколько порций вам нужно?")
     val ingredientsCount = readln().toInt()
-    for (i in ingredientsCountList.indices) {
-        ingredientsCountList[i] *= ingredientsCount
-    }
+    val newIngredientsCountList = ingredientsCountList.map{item -> item * ingredientsCount}
 
     println("На $ingredientsCount порций вам понадобится:")
-    println("Яиц – ${ingredientsCountList[0]} шт")
-    println("Молока – ${ingredientsCountList[1]} мл")
-    println("Сливочного масла – ${ingredientsCountList[2]} гр")
+    println("Яиц – ${newIngredientsCountList[0]} шт")
+    println("Молока – ${newIngredientsCountList[1]} мл")
+    println("Сливочного масла – ${newIngredientsCountList[2]} гр")
 }
