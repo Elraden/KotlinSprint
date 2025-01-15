b16c429ec0648c7f8b9ccec85b3b0799cb1915d2
Elraden <morozovsa94@yandex.ru>
2025-01-15 11:48:17 +0300

    KS-15-3 added abstract class and 2 inheritance and class. Class "administrator" has 2 own methods. Tested all methods in both objects

---
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -0,0 +1,42 @@
+package org.example.lesson_15
+
+abstract class User(
+    val name: String,
+    ) {
+    protected var message: String = ""
+
+    fun readForum() {
+        println("$name читает форум")
+    }
+
+    fun writeMessage(newMessage: String) {
+        message = newMessage
+        println("$name пишет сообщение: \"$message\"")
+    }
+}
+
+class RegularUser(name: String) : User(name) {
+}
+
+class Administrator(name: String): User(name) {
+    fun deleteMessage(messageId: Int) {
+        println("Сообщение \"$message\" c id $messageId успешно удалено")
+    }
+
+    fun deleteUser(userName: String) {
+        println("$name удаляет пользователя $userName")
+    }
+}
+
+fun main() {
+    val regularUser = RegularUser("Максим")
+    val administrator = Administrator("Инга")
+
+    regularUser.readForum()
+    regularUser.writeMessage("Привет всем")
+
+    administrator.readForum()
+    administrator.writeMessage("Я администратор. Доброго дня всем")
+    administrator.deleteUser("Alex534")
+    administrator.deleteMessage(123)
+}
