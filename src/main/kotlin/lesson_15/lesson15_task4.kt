e0e8c36f51f8277ac24a2a1fa7c7054b1e45479a
Elraden <morozovsa94@yandex.ru>
2025-01-20 21:02:11 +0300

    KS-15-4 fixed mistakes

--- src/main/kotlin/lesson_15/lesson15_task4.kt
+++ src/main/kotlin/lesson_15/lesson15_task4.kt
@@ -1,21 +1,21 @@
 package org.example.lesson_15
 
-abstract class MusicProducts(
+abstract class MusicProduct(
     val name: String,
     val amount: Int
-) {}
+)
 
 interface AccessorySearch {
     fun searchAccessories(): String
 }
 
-class Instrument(name: String, amount: Int) : MusicProducts(name, amount), AccessorySearch {
+class Instrument(name: String, amount: Int) : MusicProduct(name, amount), AccessorySearch {
     override fun searchAccessories(): String {
         return "Выполняет поиск комплектующих для инструмента: $name"
     }
 }
 
-class Accessories(name: String, amount: Int) : MusicProducts(name, amount)
+class Accessories(name: String, amount: Int) : MusicProduct(name, amount)
 
 fun main() {
     val instrument = Instrument("Электронная барабанная установка", 10)
