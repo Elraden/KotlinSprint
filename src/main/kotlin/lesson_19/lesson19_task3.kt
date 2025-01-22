894b8df84f2f9345165bbe115609fffc79b83d85
Elraden <morozovsa94@yandex.ru>
2025-01-22 12:16:03 +0300

    KS-19-3 added class with TODO in comments and TODO as error

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_19
+
+class Spaceship {
+    fun flyUp() {
+        println()
+        // TODO() Добавить логику взлета
+    }
+
+    fun land() {
+        TODO("Метод посадки еще не реализован")
+    }
+
+    fun shoot() {
+        // TODO() Добавить расчет урона
+    }
+}
+
+fun main() {
+    val spaceship = Spaceship()
+    spaceship.flyUp()
+    spaceship.land()
+    spaceship.shoot()
+}
